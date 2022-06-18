4ff1b0aa6ede291cc68a1038676b386220df03c5
Cyck0o <1357562728@qq.com>
2022-06-18 13:39:29 +0800

    c语言

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char teststring[10];
+    printf("please input your string for test\n");
+    scanf("%s",teststring);
+    printf("%s\n",teststring);
+    return 0;
+}
+
